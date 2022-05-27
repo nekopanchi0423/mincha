2c398b5c51f183e5b8c73a7b956decd215ea9d81
nekopanci <universe110423@gmail.com>
2022-05-28 00:27:23 +0900

    first commit

---
+++ kanban/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class List(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1,)
+
+    def __str__(self):
+        return self.title
+        
+class Card(models.Model):
+    title = models.CharField(max_length=200, default='ここには何も書かないで！')
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1,)
+    created_at = models.DateTimeField(default=timezone.now)
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=1,)
