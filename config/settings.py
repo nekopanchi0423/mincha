498aa152f6dd0136b49cedbbe1944ee01958e4e1
nekopanci <universe110423@gmail.com>
2022-06-03 16:36:22 +0900

    cssの修正

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    # この行を追加
 ]
 
 ROOT_URLCONF = 'config.urls'
